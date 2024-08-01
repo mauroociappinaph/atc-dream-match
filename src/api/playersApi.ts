e8efcf0862656884c54a4f1efce5708a458e99bb
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-08-01 14:20:35 -0300

    Api

--- src/api/playersApi.ts
+++ src/api/playersApi.ts
@@ -10,18 +10,24 @@ const playersApi = {
                 }
             });
             if (Array.isArray(response.data)) {
-                // Filtrar y mapear solo los jugadores que tienen imágenes no vacías
-                const players = response.data.filter(player => player.player_image && player.player_image.trim() !== '')
-                    .map(player => ({
-                        id: player.player_id,
-                        name: player.player_name,
-                        image: player.player_image,
-
-                    }));
-                return players;
+                const uniquePlayers = new Map();
+                response.data.forEach(player => {
+                    if (player.player_image && player.player_image.trim() !== '' && !uniquePlayers.has(player.player_id)) {
+                        uniquePlayers.set(player.player_id, {
+                            id: player.player_id,
+                            name: player.player_name,
+                            image: player.player_image,
+                            country: player.player_country || "Unknown",
+                            position: player.player_type,
+                            team: player.team_name
+                        });
+                    }
+                });
+                return Array.from(uniquePlayers.values());
             } else {
                 console.log('No players data found');
                 return [];
+
             }
         } catch (error) {
             console.error('Error fetching players:', error);
--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import Link from "next/link";
 import Layout from "../layout";
 import playersApi from "@/api/playersApi";
-import Button from "../../components/ui/Button";
 import { debounce } from "lodash";
+import Button from "@/components/ui/Button";
 
 const Teams = () => {
   const [teams, setTeams] = useState([
@@ -17,20 +17,26 @@ const Teams = () => {
   const [error, setError] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const playersPerPage = 6;
-
-  const indexOfLastPlayer = currentPage * playersPerPage;
-  const indexOfFirstPlayer = indexOfLastPlayer - playersPerPage;
-  const currentPlayers = availablePlayers.slice(
-    indexOfFirstPlayer,
-    indexOfLastPlayer
-  );
+  const playersMap = useMemo(() => {
+    const map = new Map();
+    availablePlayers.forEach((player) => {
+      map.set(player.id, player);
+    });
+    return map;
+  }, [availablePlayers]);
 
   const debouncedFetchPlayers = useCallback(
     debounce(async (searchTerm) => {
       setLoading(true);
       try {
         const players = await playersApi.getPlayers(searchTerm);
-        setAvailablePlayers(players);
+        const uniquePlayers = new Map();
+        players.forEach((player) => {
+          if (!uniquePlayers.has(player.id)) {
+            uniquePlayers.set(player.id, player);
+          }
+        });
+        setAvailablePlayers([...uniquePlayers.values()]);
       } catch (error) {
         setError(`Error al cargar jugadores: ${error.message}`);
       } finally {
@@ -43,26 +49,48 @@ const Teams = () => {
   useEffect(() => {
     if (searchTerm) {
       debouncedFetchPlayers(searchTerm);
+    } else {
+      setAvailablePlayers([]); // Clears the players list if the search term is empty
     }
   }, [searchTerm, debouncedFetchPlayers]);
 
-  const handleNextPage = () => {
+  const handleAddPlayerToTeam = useCallback(
+    (teamId, playerId) => {
+      setTeams((prevTeams) =>
+        prevTeams.map((team) =>
+          team.id === teamId
+            ? {
+                ...team,
+                players: [...team.players, playersMap.get(playerId)],
+              }
+            : team
+        )
+      );
+    },
+    [playersMap]
+  );
+
+  const handleNextPage = useCallback(() => {
     if (currentPage < Math.ceil(availablePlayers.length / playersPerPage)) {
       setCurrentPage((prev) => prev + 1);
     }
-  };
+  }, [currentPage, availablePlayers.length, playersPerPage]);
 
-  const handlePreviousPage = () => {
+  const handlePreviousPage = useCallback(() => {
     if (currentPage > 1) {
       setCurrentPage((prev) => prev - 1);
     }
-  };
+  }, [currentPage]);
+
+  const startIndex = (currentPage - 1) * playersPerPage;
+  const endIndex = startIndex + playersPerPage;
+  const currentPlayers = availablePlayers.slice(startIndex, endIndex);
 
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Equipos</h1>
-        <div className="flex flex-wrap justify-center gap-4">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">Jugadores</h1>
+        <div className="flex flex-wrap flex-col justify-center items-center gap-4">
           <input
             type="text"
             placeholder="Buscar jugador por nombre"
@@ -73,28 +101,36 @@ const Teams = () => {
           {loading && <div className="text-sm text-blue-600">Cargando...</div>}
           {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
         </div>
-        <div className="mt-4 w-full max-w-4xl grid grid-cols-3 gap-4">
+        <div className="mt-4 w-full max-w-4xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center align-center">
           {currentPlayers.map((player) => (
             <div
               key={player.id}
-              className="bg-white p-4 shadow rounded-lg text-center cursor-pointer"
-              onClick={() => console.log(`Selected: ${player.name}`)}
+              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden transform transition-colors hover:scale-105 dark:bg-gray-800 dark:border-gray-700"
             >
-              {player.image && (
-                <img
-                  src={player.image}
-                  alt={player.name}
-                  style={{ width: "100px", height: "100px" }}
-                />
-              )}
-              <div>{player.name}</div>
-              <div>{player.completeName}</div>
-              <Button onClick={() => handleAddPlayerToTeam(1, player.id)}>
-                Add to Team 1
-              </Button>
-              <Button onClick={() => handleAddPlayerToTeam(2, player.id)}>
-                Add to Team 2
-              </Button>
+              <img
+                className="w-full h-48 object-cover"
+                src={player.image}
+                alt={player.name}
+              />
+              <div className="p-5 text-center">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {player.name}
+                </h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {player.position}
+                </p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {player.team}
+                </p>
+                <div className="flex justify-around mt-4">
+                  <Button onClick={() => handleAddPlayerToTeam(1, player.id)}>
+                    Add to Team 1
+                  </Button>
+                  <Button onClick={() => handleAddPlayerToTeam(2, player.id)}>
+                    Add to Team 2
+                  </Button>
+                </div>
+              </div>
             </div>
           ))}
         </div>
@@ -102,7 +138,10 @@ const Teams = () => {
           <Button onClick={handlePreviousPage}>Previous</Button>
           <Button onClick={handleNextPage}>Next</Button>
         </div>
-        <Link href="/" className="mt-4 text-blue-500 hover:underline">
+        <Link
+          href="/"
+          className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+        >
           Volver al inicio
         </Link>
       </div>
